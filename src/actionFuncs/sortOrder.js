55caddc4b07d535a77ed24b6ce5fbce31b17ea2b
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-05-23 22:38:39 +0300

    sort modifed (2 sort level)

--- src/actionFuncs/sortOrder.js
+++ src/actionFuncs/sortOrder.js
@@ -1,3 +1,9 @@
 export function sortOrder(order) {
-  return order.sort((a, b) => a.articleGroupID - b.articleGroupID);
+  return order.sort((a, b) => {
+    if (a.articleGroupID - b.articleGroupID === 0) {
+      return a.article - b.article;
+    } else {
+      return a.articleGroupID - b.articleGroupID;
+    }
+  });
 }
